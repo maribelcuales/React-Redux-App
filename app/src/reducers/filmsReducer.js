c9bfd89a8e3e0d9c8075b203be4915f8c0038b08
maribelcuales <maribelcuales@gmail.com>
2020-07-22 17:15:14 +0800

    Created initial state and filmsReducer.

---
+++ app/src/reducers/filmsReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  films: [],
+  isFetching: false,
+  error: ""
+}; 
+
+export const filmsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+      return state;
+  }
+};
