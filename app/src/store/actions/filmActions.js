82ade51a52cf601b2807fda5bb5540bff180f986
maribelcuales <maribelcuales@gmail.com>
2020-07-23 00:55:38 +0800

    Created fetchFilm action dispatching axios to fetch api.

---
+++ app/src/store/actions/filmActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios'; 
+
+export const FETCH_FILM_FAILIURE =  'FETCH_FILM_FAILIURE';
+
+export const fetchFilm = () => {
+  return dispatch => {
+    dispatch({ type: 'FETCH_FILM_START' }); 
+    axios
+      .get('https://ghibliapi.herokuapp.com/films')
+      .then(res => {
+        // console.log(res); // response.data
+        dispatch({ type: 'FETCH_FILM_SUCCESS', payload: res.data })
+      })
+      .catch(error => {
+        // console.log(error); 
+        dispatch({
+          type: 'FETCH_FILM_FAILURE',
+          payload: `Error ${error.response.status}: ${error.response.data}`
+        });
+      });
+  };
+}; 
+
