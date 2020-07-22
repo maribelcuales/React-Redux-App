34befc618dcc0de5fd35920224e5f5faefde2bcc
maribelcuales <maribelcuales@gmail.com>
2020-07-22 15:30:53 +0800

    Created Films component and import it to App.

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Films from './components/Films'; 
+
 function App() {
   return (
     <div className="App">
       <h1>World of Studio Ghibli!</h1>
+      <Films />
     </div>
   );
 }
---
+++ app/src/components/Films.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+const Films = () => {
+  return (
+    <h2>Films</h2>
+
+  );
+};
+
+export default Films; 
