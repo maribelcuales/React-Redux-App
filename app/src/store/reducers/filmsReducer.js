b193e537e59e0c45aeb951a53048aad2790c216c
maribelcuales <maribelcuales@gmail.com>
2020-07-23 01:13:18 +0800

    Added case for fetching film and fetch film success in reducer.

--- app/src/store/reducers/filmsReducer.js
+++ app/src/store/reducers/filmsReducer.js
@@ -6,6 +6,18 @@ const initialState = {
 
 export const filmsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "FETCH_FILM_START":
+      return {
+        ...state,
+        isFetching: true
+      };
+    case "FETCH_FILM_SUCCESS":
+      return {
+        ...state,
+        isFetching: false,
+        films: action.payload,
+        error: ""
+      }; 
     default: 
       return state;
   }
