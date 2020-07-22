d4135bb5f9008522978dc163bb3fae0fb8d9ab0d
maribelcuales <maribelcuales@gmail.com>
2020-07-22 16:23:31 +0800

    Create store passing in rootReducer, applyMiddleware, and thunk.

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'; 
 import './index.css';
+
 import App from './App';
 
+const store = createStore(rootReducer, applyMiddleware(thunk)); 
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
