d9d3d7aafc5f9346121b427bd02020fc061d3d4c
maribelcuales <maribelcuales@gmail.com>
2020-07-23 11:09:51 +0800

    Mapped over passed in film props returning film details.

---
+++ app/src/components/Film.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Film = ({ film }) => {
+  // console.log("props", props); 
+  return (
+    <div>
+      {film.map(item => {
+        return(
+          <div key={item.id}>
+             <h3>Title: {item.title}</h3>     
+             <p>Description: {item.description}</p>
+             <p>Director: {item.director}</p>
+             <p>Producer: {item.producer}</p>
+             <p>Release Date: {item.release_date}</p>
+           </div>
+        )
+      })}
+    </div>
+  );
+};
+
+export default Film; 
