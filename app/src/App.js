0565bacf334aea7dcb7b7317d0af62af10bc7b25
maribelcuales <maribelcuales@gmail.com>
2020-07-23 12:34:07 +0800

    Added logo to header, background image, and renamed h1 title.

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.png'; 
 
 import Films from './components/Films'; 
 
 function App() {
   return (
     <div className="App">
-      <h1>World of Studio Ghibli!</h1>
-      <Films />
+      <div className="header-container">
+        <img className="logo" src={logo} alt="studio ghibli official logo"/>
+        <h1>Discover and Explore!</h1>
+        <Films />
+      </div>
     </div>
   );
 }
