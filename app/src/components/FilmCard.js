9b89755bf2426d2af1b21b151bc4f6c8c59e985c
maribelcuales <maribelcuales@gmail.com>
2020-07-23 15:01:49 +0800

    Added Reactstrap components to FilmCard.

--- app/src/components/FilmCard.js
+++ app/src/components/FilmCard.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Card,
+  CardHeader,
+  CardText,
+  CardBody,
+  CardSubtitle,
+  Badge,
+  Col
+} from "reactstrap";
+
+import './styles.css'; 
 
 const FilmCard = ({ film }) => {
   // console.log("props", props); 
   return (
-    <div>
-      {film.map(item => {
-        return(
-          <div key={item.id}>
-             <h3>Title: {item.title}</h3>     
-             <p>Description: {item.description}</p>
-             <p>Director: {item.director}</p>
-             <p>Producer: {item.producer}</p>
-             <p>Release Date: {item.release_date}</p>
-           </div>
-        )
-      })}
-    </div>
+    <Container>
+      <Row>
+          {film.map(item => {
+            return(  
+              // <div key={item.id} className="filmCard-container">
+                <Col xs="12" sm="6" md="4" xl="3">
+                  <Card className="card-details">
+                    <CardHeader>
+                      <h3>Film Title: {item.title}</h3>
+                    </CardHeader>   
+                    <CardBody>   
+                      <CardText>{item.description}</CardText>
+                      <CardSubtitle>Director: {item.director}</CardSubtitle>
+                      <CardSubtitle>Producer: {item.producer}</CardSubtitle>
+                      <Badge color="info">Release Date: {item.release_date}</Badge>
+                    </CardBody>
+                  </Card>
+                </Col>
+              // </div>
+            )
+          })}
+      </Row>
+    </Container>
   );
 };
 
